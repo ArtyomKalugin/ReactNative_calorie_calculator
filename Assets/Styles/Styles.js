54ec7667cf11a4a814fe35e96012cca30e5b3149
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-03-05 19:55:17 +0300

    Added icons

---
+++ Assets/Styles/Styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    tabBarSelectedIcon: {
+        height: 25,
+        width: 25,
+        tintColor: '#22BE54'
+    },
+    tabBarDisabledIcon: {
+        height: 25,
+        width: 25,
+        tintColor: '#858484'
+    },
+    tabBarSelectedLabel: {
+        fontSize: 12,
+        color: '#22BE54'
+    },
+    tabBarDisabledLabel: {
+        fontSize: 12,
+        color: '#858484'
+    },
+    screenBackground: {
+        color: 'white'
+    }
+})
--- Screens/NavigationScreen/NavigationScreen.js
+++ Screens/NavigationScreen/NavigationScreen.js
@@ -2,13 +2,55 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { observer } from "mobx-react";
 import {MainScreen} from "../MainScreen/MainScreen";
 import {AboutScreen} from "../AboutScreen/AboutScreen";
+import {Image, Text} from "react-native";
+import {styles} from "../../Assets/Styles/Styles";
 
 const Tab = createBottomTabNavigator();
 export const NavigationScreen = observer(() => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name={'Дневник'} component={MainScreen} />
-            <Tab.Screen name={'О приложении'} component={AboutScreen} />
+        <Tab.Navigator
+            sceneContainerStyle={{backgroundColor: 'white'}}
+        >
+            <Tab.Screen
+                name={'Дневник'}
+                component={MainScreen}
+                options={
+                    {
+                        tabBarIcon: ({focused}) => (
+                            <Image
+                                style={focused ? styles.tabBarSelectedIcon : styles.tabBarDisabledIcon}
+                                source={require('../../Assets/Icons/dictTabIcon.png')
+                            }
+                            />
+                        ),
+                        tabBarLabel: ({focused}) => (
+                            <Text style={focused ? styles.tabBarSelectedLabel : styles.tabBarDisabledLabel}>
+                                Дневник
+                            </Text>
+                        )
+                    }
+                }
+            />
+            <Tab.Screen
+                name={'О приложении'}
+                component={AboutScreen}
+                options={
+                    {
+                        tabBarIcon: ({focused}) => (
+                            <Image
+                                style={focused ? styles.tabBarSelectedIcon : styles.tabBarDisabledIcon}
+                                source={require('../../Assets/Icons/aboutTabIcon.png')
+                                }
+                            />
+                        ),
+                        tabBarLabel: ({focused}) => (
+                            <Text style={focused ? styles.tabBarSelectedLabel : styles.tabBarDisabledLabel}>
+                                О приложении
+                            </Text>
+                        )
+                    }
+                }
+            />
         </Tab.Navigator>
     );
 })
